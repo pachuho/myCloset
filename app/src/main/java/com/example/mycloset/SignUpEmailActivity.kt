dc81d7510e8f83e6452716b524c6482d1e9d6d2c
pachuho <blueblack969@naver.com>
2020-11-23 13:21:27 +0900

    add function(helper, length)

--- app/src/main/java/com/example/mycloset/SignUpEmailActivity.kt
+++ app/src/main/java/com/example/mycloset/SignUpEmailActivity.kt
@@ -5,6 +5,8 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.TypedValue
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -31,17 +33,7 @@ class SignUpEmailActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
-                if (et_email.length() > 0 && !android.util.Patterns.EMAIL_ADDRESS.matcher(et_email.text.toString()).matches())
-                {
-                    ll_email.removeAllViews()
-                    val emailHelper = TextView(this@SignUpEmailActivity)
-                    emailHelper.text = " 이메일 주소를 다시 확인 해주세요"
-                    emailHelper.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    emailHelper.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_email.addView(emailHelper)
-                } else {
-                    ll_email.removeAllViews()
-                }
+                addHelper(et_email.length() > 0 && !android.util.Patterns.EMAIL_ADDRESS.matcher(et_email.text.toString()).matches(), ll_email, "이메일 주소를 다시 확인 해주세요")
             }
         })
 
@@ -50,36 +42,14 @@ class SignUpEmailActivity : AppCompatActivity() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                ll_pwdLength.removeAllViews()
-                val pwdLength = TextView(this@SignUpEmailActivity)
-                pwdLength.text = "${et_pwd.text.length}/16"
-                pwdLength.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10F)
-                ll_pwdLength.addView(pwdLength)
+                addLength(ll_pwdLength, et_pwd, "16")
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                if (et_pwd.length() in 1..7) {
-                    ll_pwd.removeAllViews()
-                    val pwdHelper = TextView(this@SignUpEmailActivity)
-                    pwdHelper.text = " 비밀번호 8~16자를 입력해주세요"
-                    pwdHelper.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    pwdHelper.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_pwd.addView(pwdHelper)
-                } else {
-                    ll_pwd.removeAllViews()
-                }
+                addHelper(et_pwd.length() in 1..7, ll_pwd, "비밀번호 8~16자를 입력해주세요")
 
                 // 비밀번호 확인 입력 후 비밀번호 입력창 입력 시
-                if(et_pwdConfirm.length() > 0 && et_pwd.text.toString() != et_pwdConfirm.text.toString()){
-                    ll_pwdConfirm.removeAllViews()
-                    val pwdConfirm = TextView(this@SignUpEmailActivity)
-                    pwdConfirm.text = " 비밀번호가 일치하지 않아요"
-                    pwdConfirm.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    pwdConfirm.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_pwdConfirm.addView(pwdConfirm)
-                } else {
-                    ll_pwdConfirm.removeAllViews()
-                }
+                addHelper(et_pwdConfirm.length() > 0 && et_pwd.text.toString() != et_pwdConfirm.text.toString(), ll_pwdConfirm, "비밀번호가 일치하지 않아요")
             }
         })
 
@@ -88,24 +58,11 @@ class SignUpEmailActivity : AppCompatActivity() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                ll_pwdConfirmLength.removeAllViews()
-                val pwdConfirmLength = TextView(this@SignUpEmailActivity)
-                pwdConfirmLength.text = "${et_pwdConfirm.text.length}/16"
-                pwdConfirmLength.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10F)
-                ll_pwdConfirmLength.addView(pwdConfirmLength)
+                addLength(ll_pwdConfirmLength, et_pwdConfirm, "16")
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                if(et_pwdConfirm.length() > 0 && et_pwd.text.toString() != et_pwdConfirm.text.toString()) {
-                    ll_pwdConfirm.removeAllViews()
-                    val pwdConfirm = TextView(this@SignUpEmailActivity)
-                    pwdConfirm.text = " 비밀번호가 일치하지 않아요"
-                    pwdConfirm.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    pwdConfirm.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_pwdConfirm.addView(pwdConfirm)
-                } else {
-                    ll_pwdConfirm.removeAllViews()
-                }
+                addHelper(et_pwdConfirm.length() > 0 && et_pwd.text.toString() != et_pwdConfirm.text.toString(), ll_pwdConfirm, "비밀번호가 일치하지 않아요")
             }
         })
 
@@ -114,25 +71,11 @@ class SignUpEmailActivity : AppCompatActivity() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                ll_nickNameLength.removeAllViews()
-                val nickNameLength = TextView(this@SignUpEmailActivity)
-                nickNameLength.text = "${et_nickName.text.length}/8"
-                nickNameLength.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10F)
-                ll_nickNameLength.addView(nickNameLength)
+                addLength(ll_nickNameLength, et_nickName, "8")
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                if (et_nickName.length() == 1 || !et_nickName.text.matches(symbolNickname.toRegex()))
-                {
-                    ll_nickName.removeAllViews()
-                    val nicknameHelper = TextView(this@SignUpEmailActivity)
-                    nicknameHelper.text = " 특수문자 제외 2~8자를 입력해주세요"
-                    nicknameHelper.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    nicknameHelper.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_nickName.addView(nicknameHelper)
-                } else {
-                    ll_nickName.removeAllViews()
-                }
+                addHelper(et_nickName.length() == 1 || !et_nickName.text.matches(symbolNickname.toRegex()), ll_nickName, "특수문자 제외 2~8자를 입력해주세요")
             }
         })
 
@@ -143,21 +86,9 @@ class SignUpEmailActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
-                if (et_birthday.length() > 1 && !et_birthday.text.matches(symbolBirthday.toRegex()))
-                {
-                    ll_birthday.removeAllViews()
-                    val helper = TextView(this@SignUpEmailActivity)
-                    helper.text = " 생년월일을 다시 확인 해주세요"
-                    helper.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
-                    helper.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
-                    ll_birthday.addView(helper)
-                } else {
-                    ll_birthday.removeAllViews()
-                }
+                addHelper(et_birthday.length() > 1 && !et_birthday.text.matches(symbolBirthday.toRegex()), ll_birthday, "생년월일을 다시 확인 해주세요")
             }
-
         })
-
     }
 
     // 툴바 뒤로 가기 클릭 시
@@ -179,4 +110,24 @@ class SignUpEmailActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right)
     }
 
+    // EditText 하단 도움말
+    fun addHelper(condition : Boolean, layout: LinearLayout, inputText: String? = "텍스트를 입력해주세요") {
+        if (condition) {
+            layout.removeAllViews()
+            val helper = TextView(this@SignUpEmailActivity)
+            helper.text = " $inputText"
+            helper.setTextColor(ContextCompat.getColor(this@SignUpEmailActivity, R.color.red))
+            helper.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11F)
+            layout.addView(helper)
+        } else layout.removeAllViews()
+    }
+
+    // EditText 글자 수 체크
+    fun addLength(layout : LinearLayout, editName : EditText, maxLength : String? = "8") {
+        layout.removeAllViews()
+        val checkLength = TextView(this@SignUpEmailActivity)
+        checkLength.text = "${editName.text.length}/${maxLength}"
+        checkLength.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10F)
+        layout.addView(checkLength)
+    }
 }
